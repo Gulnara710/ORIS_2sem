28904b95e02262728b917c86dabd9ec4d71ee1d6
Gulnara710 <gulnaranazipova70262@gmail.com>
2026-03-20 23:36:15 +0300

    homework_4

---
+++ src/main/java/ru/itis/persistence/entity/OrderEntity.java
@@ -0,0 +1,21 @@
+package ru.itis.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
+public class OrderEntity extends OrisBaseEntity {
+
+    private String description;
+    private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
--- src/main/java/ru/itis/persistence/entity/UserEntity.java
+++ src/main/java/ru/itis/persistence/entity/UserEntity.java
@@ -1,17 +1,28 @@
 package ru.itis.persistence.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity {
-    private UUID id;
+public class UserEntity extends OrisBaseEntity {
     private String name;
     private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private UserStatus status;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderEntity> orders = new ArrayList<>();
 }
